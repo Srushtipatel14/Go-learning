b019add307d3e3476fbd988bfbaed2582a30b942
Srushtipatel14 <srushtip579@gmail.com>
2026-05-25 21:32:40 +0530

    simple hello world program

--- day1/main.go
+++ day1/main.go
@@ -3,12 +3,5 @@ package main
 import "fmt"
 
 func main() {
-	a := 5
-
-	for i := 1; i <= a; i++ {
-		for j := 1; j <= i; j++ {
-			fmt.Print("*")
-		}
-		fmt.Println()
-	}
+	fmt.Println(("Hello world"))
 }
