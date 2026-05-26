bbb07fc9beff2606d812c2b8b50affccf42a1e4a
Srushtipatel14 <srushtip579@gmail.com>
2026-05-26 21:38:21 +0530

    pointers

--- day1/main.go
+++ day1/main.go
@@ -4,17 +4,13 @@ import (
 	"fmt"
 )
 
+func printVal(val *int) {
+	*val = 20
+	fmt.Println(*val)
+}
+
 func main() {
-	val := make(map[string]interface{})
-
-	val["one"] = 1
-	val["two"] = 2.7
-	val["three"] = true
-	val["four"] = "four"
-	val["five"] = []string{"five", "six", "seven"}
-	val["six"]=map[string]interface{}{
-		"eight": 8,
-	}
+	val := 200
+	printVal(&val)
 	fmt.Println(val)
-
 }
